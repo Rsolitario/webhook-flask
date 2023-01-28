e4bc2c9d89f8563c9b99e8fd809b89f9a8765845
eventos-dev <caruasdo@gmail.com>
2023-01-28 11:19:36 -0400

    Error 500

--- app.py
+++ app.py
@@ -6,10 +6,10 @@ app = Flask(__name__)
 webhook_site = 'https://webhook.site/53aaf13b-194a-4e47-ab25-5f039b30d235'
 
 
-@app.route("/ebhook", methods=['POST'])
+@app.route("/webhook", methods=['POST'])
 def webhook():
     if request.method == 'POST':
-        data = {'webhook_test': request.json}
+        data = request.json
         request.post(webhook_site, data=json.dumps(data),
                      headers={'Content-Type: application/json'})
         return 'success', 200
