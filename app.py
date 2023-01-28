e64975c99afd471cfa7b1ec1efee864edd5c71a5
eventos-dev <caruasdo@gmail.com>
2023-01-28 11:15:13 -0400

    response webhook site

--- app.py
+++ app.py
@@ -1,18 +1,25 @@
 from flask import Flask, request, abort
 import os
+import json
 app = Flask(__name__)
+
+webhook_site = 'https://webhook.site/53aaf13b-194a-4e47-ab25-5f039b30d235'
+
 
 @app.route("/ebhook", methods=['POST'])
 def webhook():
     if request.method == 'POST':
-        print(request.json)
+        data = {'webhook_test': request.json}
+        request.post(webhook_site, data=json.dumps(data),
+                     headers={'Content-Type: application/json'})
         return 'success', 200
     else:
         abort(400)
+
 
 @app.route("/main")
 def home():
     return 'hola mundo'
 
-#if __name__ == '__main__':
+# if __name__ == '__main__':
 #    app.run(host='0.0.0.0', port=int(os.environ.get('PORT', 8080)))
