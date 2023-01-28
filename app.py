395d6814ddf8e650e63ec67bf89c4f2113ec8e55
eventos-dev <caruasdo@gmail.com>
2023-01-28 10:56:07 -0400

    eliminando arranque

--- app.py
+++ app.py
@@ -14,5 +14,5 @@ def webhook():
 def home():
     return 'hola mundo'
 
-if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=int(os.environ.get('PORT', 8080)))
+#if __name__ == '__main__':
+#    app.run(host='0.0.0.0', port=int(os.environ.get('PORT', 8080)))
