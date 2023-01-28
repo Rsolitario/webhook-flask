a0336410540c4c2c78e07d2e23a0e4499efdafa4
eventos-dev <caruasdo@gmail.com>
2023-01-28 11:22:11 -0400

    Error 500

--- app.py
+++ app.py
@@ -9,7 +9,7 @@ webhook_site = 'https://webhook.site/53aaf13b-194a-4e47-ab25-5f039b30d235'
 @app.route("/webhook", methods=['POST'])
 def webhook():
     if request.method == 'POST':
-        data = request.json
+        data = json.loads(request.json)
         request.post(webhook_site, data=json.dumps(data),
                      headers={'Content-Type: application/json'})
         return 'success', 200
