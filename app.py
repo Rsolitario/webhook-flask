d7dd4d4c62cd06812cbb4a5058ca73cf4e4b4daf
eventos-dev <caruasdo@gmail.com>
2023-01-28 11:43:15 -0400

    error reparado

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 from flask import Flask, request, abort
 import os
 import json
+import requests
 app = Flask(__name__)
 
 webhook_site = 'https://webhook.site/53aaf13b-194a-4e47-ab25-5f039b30d235'
@@ -9,9 +10,10 @@ webhook_site = 'https://webhook.site/53aaf13b-194a-4e47-ab25-5f039b30d235'
 @app.route("/webhook", methods=['POST'])
 def webhook():
     if request.method == 'POST':
-        data = json.loads(request.json)
-        request.post(webhook_site, data=json.dumps(data),
-                     headers={'Content-Type: application/json'})
+        data = request.json
+        print(json.dumps(data))
+        requests.post(webhook_site, data=json.dumps(data),
+                     headers={"Content-Type": "application/json"})
         return 'success', 200
     else:
         abort(400)
@@ -21,5 +23,5 @@ def webhook():
 def home():
     return 'hola mundo'
 
-# if __name__ == '__main__':
-#    app.run(host='0.0.0.0', port=int(os.environ.get('PORT', 8080)))
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=int(os.environ.get('PORT', 8080)))
