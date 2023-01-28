75f4ecadc2b340220e85fe312ecaac9523fd8f3a
eventos-dev <caruasdo@gmail.com>
2023-01-28 10:44:46 -0400

    webhook

---
+++ app.py
@@ -0,0 +1,18 @@
+from flask import Flask, request, abort
+import os
+app = Flask(__name__)
+
+@app.route("/ebhook", methods=['POST'])
+def webhook():
+    if request.method == 'POST':
+        print(request.json)
+        return 'success', 200
+    else:
+        abort(400)
+
+@app.route("/")
+def home():
+    return 'hola mundo'
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=int(os.environ.get('PORT', 8080)))
